e5fcd4ee5e34ff0b3bc0c951ef8186e49ab43864
Edgar Doiron <edgar.doiron@gmail.com>
2016-07-03 01:38:58 -0400

    added font-awesome and added colors for the required fonts

--- src/app/pages/dashboard/pieChart/pieChart.service.ts
+++ src/app/pages/dashboard/pieChart/pieChart.service.ts
@@ -12,24 +12,60 @@ export class PieChartService {
     return [
       {
         color: pieColor,
-        description: 'New Visits',
+        description: 'Analyse',
         stats: '57,820',
-        icon: 'person',
+        icon: 'fa-cart-plus',
+        coloricon: 'analyse',
+        hoverdesc: 'Analyse pour évaluation terminé',
       }, {
         color: pieColor,
-        description: 'Purchases',
+        description: 'Dev',
         stats: '$ 89,745',
-        icon: 'money',
+        icon: 'fa-code',
+        coloricon: 'dev',
+        hoverdesc: 'En développement',
       }, {
         color: pieColor,
-        description: 'Active Users',
-        stats: '178,391',
-        icon: 'face',
-      }, {
-        color: pieColor,
-        description: 'Returned',
+        description: 'Support',
         stats: '32,592',
-        icon: 'refresh',
+        icon: 'fa-comments-o',
+        coloricon: 'support-analyste',
+        hoverdesc: 'Demande de support à l\'analyste',
+      }, {
+        color: pieColor,
+        description: 'Dev Terminé',
+        stats: '32,592',
+        icon: 'fa-cloud-upload',
+        coloricon: 'dev-termine',
+        hoverdesc: 'En cours de test hors-dev',
+      }, {
+        color: pieColor,
+        description: 'Urgent',
+        stats: '32,592',
+        icon: 'fa-fire',
+        coloricon: 'urgent-asap',
+        hoverdesc: 'Demande urgente',
+      }, {
+        color: pieColor,
+        description: 'En Attente',
+        stats: '178,391',
+        icon: 'fa-spinner',
+        coloricon: 'en-attente',
+        hoverdesc: 'En attente ou sur la glace',
+      }, {
+        color: pieColor,
+        description: 'PROD',
+        stats: '32,592',
+        icon: 'fa-key',
+        coloricon: 'prod',
+        hoverdesc: 'Demande en prod',
+      }, {
+        color: pieColor,
+        description: 'Approuvé',
+        stats: '32,592',
+        icon: 'fa-envelope-o',
+        coloricon: 'confirme-par-chef',
+        hoverdesc: 'Confirmation du Chef d\'équipe pour débuter',
       }
     ];
   }
